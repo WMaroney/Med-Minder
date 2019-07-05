21caa87ec0f5dd6678f0d11f5d01a158a1fc91b6
William Maroney <32745456+WMaroney@users.noreply.github.com>
2019-07-05 11:51:13 -0400

    Create med_minder_schema.sql

---
+++ med_minder_schema.sql
@@ -0,0 +1,19 @@
+SET foreign_key_checks = 0;
+drop table if exists users;
+create table users (
+	user_id integer primary key auto_increment,
+	user_email varchar(255) not null,
+	user_password varchar(255) not null
+);
+
+drop table if exists usermeds;
+create table usermeds (
+	user_id integer primary key auto_increment,
+	med_name varchar(255) not null,
+	med_freq varchar(255) not null,
+	med_dose varchar(255) not null,
+	dr_name varchar(255) not null,
+	refill_date varchar(255) not null,
+);
+ALTER TABLE usermeds ADD FOREIGN KEY (user_id) references users(user_id);
+SET foreign_key_checks = 1;
