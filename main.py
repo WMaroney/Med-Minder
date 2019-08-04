2bf6a3ae2b64dbb715c6b7ddedbbc1a118a6cf58
William Maroney <32745456+WMaroney@users.noreply.github.com>
2019-08-04 13:20:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ import os
 import datetime
 import re
 
-db = pymysql.connect(host='35.229.79.169', user='root', password='password', db='med_minder')
-c = db.cursor()
+#db = pymysql.connect(host='35.229.79.169', user='root', password='password', db='med_minder')
+#c = db.cursor()
 
 ## FLASK FORMS subclassed from FlaskForm##
 
