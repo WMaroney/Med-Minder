caf46960ce45564e1694349394821c7f860a28e8
William Maroney <32745456+WMaroney@users.noreply.github.com>
2019-08-13 09:03:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ def signup():
 def addscan ():
 	return render_template('addscan.html')
 	
-@app.route('/imgprocess', methods=['GET', 'POST'])
+@app.route('/imgprocess', methods=['POST'])
 def imgprocess():
 	data = request.form["img"] # grab the image captured
 	image_data = re.sub('^data:image/.+;base64,', '', request.form['img']) # remove metadata (mimetype)
