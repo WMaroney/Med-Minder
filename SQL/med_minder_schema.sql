037dee58c7ff4c2f782bdf7738dc9c9feff807eb
William Maroney <32745456+WMaroney@users.noreply.github.com>
2019-08-19 15:37:30 -0400

    Update med_minder_schema.sql

--- SQL/med_minder_schema.sql
+++ SQL/med_minder_schema.sql
@@ -1,21 +1,26 @@
 SET foreign_key_checks = 0;
 drop table if exists users;
 create table users (
-	user_id integer primary key auto_increment,
-	user_email varchar(255)unique key not null,,
-	user_password varchar(255)
+	user_id integer auto_increment,
+	user_email varchar(255)unique key not null,
+	user_password varchar(255),
+	PRIMARY KEY (user_id)
 );
 
 drop table if exists usermeds;
 create table usermeds (
-	med_id integer primary key auto_increment,
+	med_id integer auto_increment,
 	user_id integer,
 	med_name varchar(255),
 	med_freq varchar(255),
 	med_dose varchar(255),
 	dr_name varchar(255),
 	refill_date varchar(255),
-	num_refills integer
+	num_refills integer,
+	PRIMARY KEY (med-id),
+	FOREIGN KEY fk_user(user_id) REFERENCES users(user_id)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT
 );
-ALTER TABLE usermeds ADD FOREIGN KEY (user_id) references users(user_id);
 SET foreign_key_checks = 1;
+
